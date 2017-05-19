c2eec7410836dbe8ba2e017c2979c56564024839
Alberto Martinez Menendez <amartinez95zgz@gmail.com>
2017-05-19 17:07:52 +0200

    Barra de busqueda funcional añadida a Mis Comunidades

--- APSocial/public/javascripts/home/homeController.js
+++ APSocial/public/javascripts/home/homeController.js
@@ -4,6 +4,7 @@ angular.module('APSocial').controller('HomeCtrl', [
                 'auth',
                 function($scope, communities, auth){
                     $scope.allCommunities = communities.communities;
+                    $scope.allMyCommunities = communities.communities;
                     $scope.communities = communities.communities;
                     $scope.mycommunities = communities.mycommunities;
                     $scope.isLoggedIn = auth.isLoggedIn;
@@ -34,11 +35,20 @@ angular.module('APSocial').controller('HomeCtrl', [
                         return true;
                     }
 
-                    $scope.search = function() {
+                    $scope.searchInCommunities = function() {
                         $scope.communities = [];
                         for (var i = 0; i< $scope.allCommunities.length; i++) {
                             if ($scope.allCommunities[i].name.toUpperCase().includes($scope.stringSearch.toUpperCase())) {
                                 $scope.communities.push($scope.allCommunities[i]);
+                            }
+                        }
+                    }
+
+                    $scope.searchInMyCommunities = function() {
+                        $scope.mycommunities = [];
+                        for (var i = 0; i< $scope.allMyCommunities.length; i++) {
+                            if ($scope.allMyCommunities[i].name.toUpperCase().includes($scope.stringSearchMy.toUpperCase())) {
+                                $scope.mycommunities.push($scope.allMyCommunities[i]);
                             }
                         }
                     }
